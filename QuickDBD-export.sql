e6082ce6dcbb728d3f41adc33553b975fbed83d2
Giacomo Siciliani <giacomosiciliani@Giacomos-MBP.localdomain>
2024-01-04 19:18:53 -0600

    adding ERD

---
+++ QuickDBD-export.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "blurb" VARCHAR(50)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" VARCHAR(50)   NOT NULL,
+    "currency" VARCHAR(50)   NOT NULL,
+    "launch_date" int   NOT NULL,
+    "end_date" VARCHAR(50)   NOT NULL,
+    "category_id" VARCHAR(50)   NOT NULL,
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" VARCHAR(50)   NOT NULL,
+    "category_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
